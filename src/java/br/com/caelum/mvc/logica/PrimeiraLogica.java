2ddf7929883dab09a263b09776d472eac25deb11
hallef.wantek <hallef.wantek@E00009584.tiradentes.local>
2018-11-29 17:55:11 -0200

    Corrigindo lógica

--- src/java/br/com/caelum/mvc/logica/PrimeiraLogica.java
+++ src/java/br/com/caelum/mvc/logica/PrimeiraLogica.java
@@ -4,8 +4,9 @@ package br.com.caelum.mvc.logica;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class PrimeiraLogica {
+public class PrimeiraLogica implements Logica {
 
+    @Override
     public String executa(HttpServletRequest req,HttpServletResponse res) throws Exception {
         System.out.println("Executando a logica	...");
         System.out.println("Retornando o nome da página	JSP ...");
